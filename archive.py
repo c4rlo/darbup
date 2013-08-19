06e8cada614b80801331b41909cebd1536344062
Carlo Teubner <carlo.teubner@gmail.com>
2013-08-19 22:36:45 +0100

    ArchiveSet done(?); config done

--- archive.py
+++ archive.py
@@ -2,6 +2,7 @@ import re, os, os.path
 from datetime import datetime
 from operator import attrgetter
 
+from errors import BackupError
 
 ARCHIVE_BASENAME_SUFFIX_PAT = \
         r'-(\d\d\d\d)-(\d\d)-(\d\d)-(\d\d)(\d\d)-(full|incr)\.1\.dar$'
@@ -11,42 +12,53 @@ class Archive:
     BASENAME_SUFFIX_RE = re.compile(ARCHIVE_BASENAME_SUFFIX_PAT)
 
     def __init__(self, basedir, basename, match):
-        self.basedir = basedir
-        self.basename = basename
+        self._basedir = basedir
+        self._basename = basename
         self.timestamp = datetime(int(match.group(1)), int(match.group(2)),
                                   int(match.group(3)), int(match.group(4)),
                                   int(match.group(5)))
-        self.difftype = match.group(6)
+        self.is_incremental = (match.group(6) == 'incr')
         self.size = os.path.getsize(self.path())
+        self._prev = self._next = None
 
     def path(self):
-        return os.path.join(self.basedir, self.basename)
+        return os.path.join(self._basedir, self._basename)
 
 
-class ArchiveLister:
-    def __init__(self, name):
-        self.name = name
-
-    def get_archives(self, path):
+class ArchiveSet:
+    def __init__(self, name, path):
+        self._name = name
         archives = [ ]
         for fn in os.listdir(path):
             archive = self._archive_at_path(path, fn)
             if archive:
                 archives.append(archive)
         archives.sort(key=attrgetter('timestamp'))
-        previous = None
-        for archive in archives:
-            if archive.difftype == 'incr':
-                if not previous:
-                    raise BackupException('Oldest archive {} is '
-                                          'incremental'.format(archive.path()))
-            archive.reference = previous
-            previous = archive
-        return archives
+        if len(archives) > 0 and archives[0].is_incremental:
+            raise BackupError('Oldest archive {} is incremental'
+                              .format(archive.path()))
+        prev = None
+        for arch in archives:
+            if prev:
+                prev._next = arch
+                arch._prev = prev
+            else:
+                self._first = arch
+            prev = arch
+        self._last = prev
+
+    def __iter__(self):
+        curr = self._first
+        while curr:
+            yield curr
+            curr = curr._next
+
+    def latest(self):
+        return self._last
 
     def _archive_at_path(self, basedir, basename):
-        if basename.startswith(self.name):
-            m = BASENAME_SUFFIX_RE.match(basename[len(self.name):])
+        if basename.startswith(self._name):
+            m = BASENAME_SUFFIX_RE.match(basename[len(self._name):])
             if m:
                 return Archive(basedir, basename, m)
         return None
---
+++ config.py
@@ -0,0 +1,65 @@
+import configparser, os.path, re
+
+import schedules, rmpolicies
+from errors import BackupError
+
+class Instance:
+    def __init__(self, name, dest_dir, dar_args, capacity, full_intvl,
+                 incr_intvl, rm_policy):
+        self.name = name
+        self.dest_dir = dest_dir
+        self.dar_args = dar_args
+        self.capacity = capacity
+        self.full_intvl = full_intvl
+        self.incr_intvl = incr_intvl
+        self.rm_policy = rm_policy
+
+class Config:
+    def __init__(self):
+        parser = configparser.ConfigParser()
+        parser['DEFAULT'] = {
+            'FullBackupsInterval': 'monthly',
+            'IncrBackupsInterval': 'daily',
+            'RemovalPolicy': 'fifo'
+        }
+        parser.read((
+            os.path.join(os.environ['HOME'], '.regudar', 'config'),
+            '/etc/regudar.conf'
+        ))
+
+        self.instances = []
+
+        for name, section in parser:
+            self.instances.append(Instance(
+                name,
+                self._required_value(section, name, 'DestinationDir'),
+                self._required_value(section, name, 'DarArguments'),
+                self._get_capacity_value(section, name),
+                schedules.schedule_by_name(section['FullBackupsInterval']),
+                schedules.schedule_by_name(section['IncrBackupsInterval']),
+                rmpolicies.rmpolicy_by_name(section['RemovalPolicy'])))
+
+    def _required_value(self, section, section_name, name):
+        if name not in section:
+            raise BackupError('Configuration file section {} is missing '
+                              'required setting "{}"'.format(section_name,
+                                                             name))
+        return section[name]
+
+    CAPA_RE = re.compile(r'[0-9]+[kmgtp]$')
+    CAPA_SUFFIX_FACTORS = {
+        'k': (1 << 10),
+        'm': (1 << 20),
+        'g': (1 << 30),
+        't': (1 << 40),
+        'p': (1 << 50)
+    }
+
+    def _get_capacity_value(self, section, section_name):
+        s = self._required_value(section, section_name, 'Capacity')
+        m = CAPA_RE.match(s, re.IGNORECASE)
+        if not m:
+            raise BackupError('Configuration file section {} has bad Capacity '
+                              'value "{}": must match /^{}/'.format(
+                                  section_name, s, CAPA_RE.pattern))
+        return int(s[:-1]) * CAPA_SUFFIX_FACTORS[s[-1]]
